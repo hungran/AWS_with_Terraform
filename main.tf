a87652677ca76d9978818e03befc237d1f3ba3c2
hungran <vmhung290791@gmail.com>
2020-01-29 01:29:12 +0700

    update_main.tf

--- main.tf
+++ main.tf
@@ -7,4 +7,3 @@ resource "aws_key_pair" "hung" {
   key_name = "hung"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEA1Jy1w8BSeJoKIPpWmMIjSStZdmSyreJbp72MXu7ltNiGke4qJGMYfPMM5vNQvNBJuA6Ra/C2wjF7IbsDMwVbxynaaglj1ECSbYF/b8xgXqpChhSERjtl/VTEJxFgp/HkvhRuXYxfFT2W9OTJATqd8BdxtJ4l+zWpVaArSJ2Ex4o+yLzWLGjOaEyabl/33sxKQUAk67iSTfEVeYRs6S1EMLMeV5p19GGQN7Jlmv3s3rbup26bk42n5SKt4rVppe17ZP1LlzKLrl/+WRhrsDMNjIngJ7HLv+S+q8E52iVN+E5cMxSnFjIaC2kI9Tig/v9+bhmqnwyxcwWKVcJJKl8uqQ=="
 }
-
