7d91aa8dc09914188df4d3bf7f0ac706a471d3a8
hungran <vmhung290791@gmail.com>
2020-02-03 00:28:40 +0700

    new file:   main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  shared_credentials_file = ".aws/Cred/accessKeys.csv"
+  region                  = "ap-southeast-1"
+}
+
+module "vpc" {
+  source        = "./vpc"
+  vpc_cidr      = "192.168.0.0/16"
+  public_cidrs  = ["192.168.1.0/24", "192.168.2.0/24"]
+  private_cidrs = ["192.168.253.0/24", "192.168.254.0/24"]
+}
+module "ec2" {
+  source         = "./ec2"
+  security_group = "${module.vpc.security_group}"
+  subnets        = "${module.vpc.public_subnet}"
+}
