3de01d72214c7ae744c049890531a71eaa313ae7
hungran <vmhung290791@gmail.com>
2020-02-03 13:53:12 +0700

    update

--- ec2/main.tf
+++ ec2/main.tf
@@ -13,8 +13,8 @@ data "template_file" "bootstrap" {
 resource "aws_instance" "hung_terraform_ubuntu" {
   count = 2
   ami   = "ami-0ee0b284267ea6cde" //ubuntu 16.04 LTS
-  instance_type = "t2.micro"
-  key_name = "public_key"
+  instance_type = "${var.instance_type}"
+  key_name = "${aws_key_pair.public_key.id}"
   vpc_security_group_ids = ["${var.security_group}"]
   subnet_id = "${element(var.subnets, count.index )}"
   user_data = "${(data.template_file.bootstrap.rendered)}"
