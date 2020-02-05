ac5106eea26bece0eb516485204d111f725824fd
hungran <vmhung290791@gmail.com>
2020-02-05 17:16:22 +0700

    update-deleteontermination

--- ec2/main.tf
+++ ec2/main.tf
@@ -18,9 +18,27 @@ resource "aws_instance" "hung_terraform_ubuntu" {
   vpc_security_group_ids = ["${var.security_group}"]
   subnet_id = "${element(var.subnets, count.index )}"
   user_data = "${(data.template_file.bootstrap.rendered)}"
-  //depends_on = ["aws_security_group.hung_sg", "aws_subnet.public_subnet"]
-  
+  delete_on_termination = true
   tags = {
     Name = "Canh-Rau-Den-${count.index +1}"
   }
+}
+resource "aws_ebs_volume" "hung-ebs" {
+  count       = 2
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}" // query lần lượt và tạo 2 ebs lần lượt vào 2 Available AZ
+  size = 1 // GiB
+  type = "gp2"
+  
+  tags = {
+    Name = "hung-ebs-${count.index +1}"
+  }
+}
+//attach volume vào instance
+resource "aws_volume_attachment" "ebs-vol-attach" {
+  count = 2
+  device_name = "/dev/xvdh"
+  instance_id = "${aws_instance.hung_terraform_ubuntu.*.id[count.index]}" //attach lần lượt vào 2 instance vừa được tạo
+  volume_id = "${aws_ebs_volume.hung-ebs.*.id[count.index]}"
+  depends_on = ["aws_ebs_volume.hung-ebs", "aws_instance.hung_terraform_ubuntu"]
+  
 }
