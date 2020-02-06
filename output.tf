1f909b6896e53aa6564287eec41ced897e440371
hungran <vmhung290791@gmail.com>
2020-02-06 16:21:32 +0700

    update

--- output.tf
+++ output.tf
@@ -1,3 +1,12 @@
 output "elb_dns_name" {
   value = "${module.elb.elb_dns_name}"
 }
+/*
+output "instance1_id" {
+  value = "${element(module.ec2.instance1_id)}"
+}
+
+output "instance2_id" {
+  value = "${element(module.ec2.instance2_id)}"
+}
+*/
