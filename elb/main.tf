5d5640af4fc0578119ac7159322fb0e563a0c11f
hungran <vmhung290791@gmail.com>
2020-02-06 16:27:28 +0700

    MarkComment_lb_target_group_attachment

--- elb/main.tf
+++ elb/main.tf
@@ -18,20 +18,21 @@ resource "aws_lb_target_group" "hung-target-group" {
     target_type = "instance"
     vpc_id      = "${var.vpc_id}"
 }
-resource "aws_lb_target_group_attachment" "hung_lb_target_group_attach_1" {
+/* resource "aws_lb_target_group_attachment" "hung_lb_target_group_attach_1" {
     target_group_arn    = "${aws_lb_target_group.hung-target-group.arn}"
     target_id           = "${var.instance1_id}"
     port                = 80
 }
-resource "aws_lb_target_group_attachment" "hung_lb_target_group_attach_2" {
+ resource "aws_lb_target_group_attachment" "hung_lb_target_group_attach_2" {
     target_group_arn    = "${aws_lb_target_group.hung-target-group.arn}"
     target_id           = "${var.instance2_id}"
     port                = 80
-}
+ } */
+ 
 resource "aws_lb" "hung_lb" {
     name    = "hung-lb-by-terraform"
     internal = false
-    security_groups = ["${var.security_group}"] // use hung-sg from module vpc
+    security_groups = ["${var.security_groups}"] // use hung-sg from module vpc
     subnets = [
         "${var.subnet1}", //public subnet 1 from module vpc
         "${var.subnet2}", //public subnet 2 from module vpc
