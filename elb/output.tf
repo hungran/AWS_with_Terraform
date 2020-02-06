903f7685ca64118550461202c2d37931a14021c6
hungran <vmhung290791@gmail.com>
2020-02-06 16:28:06 +0700

    Fix_typing_at_output_target_group_arn

--- elb/output.tf
+++ elb/output.tf
@@ -2,6 +2,6 @@ output "elb_dns_name" {
   value = "${aws_lb.hung_lb.dns_name}"
 }
 
-output "elb_target_group_arn" {
+output "target_group_arn" {
   value = "${aws_lb_target_group.hung-target-group.arn}"
 }
